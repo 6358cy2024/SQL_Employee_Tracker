d26f99b3f40ad7ee438bfa22efd675c389c88dbd
Chris Yanez <cy2019@hotmail.com>
2024-07-29 19:52:35 -0400

    View Works

--- db/seed.sql
+++ db/seed.sql
@@ -1,8 +1,26 @@
 INSERT INTO department (dName) VALUES
-    ('Sales');
+    ('Sales'),
+    ('Finance'),
+    ('Engineering'),
+    ('Legal');
 
 INSERT INTO roles (title, salary, department_id) VALUES
-    ('Sales Lead', 120000, 1);
+    ('Sales Lead', 120000, 1), --1
+    ('Salesperson', 100000, 1), --2
+    ('Lead Engineer', 150000, 2), --3
+    ('Software Engineer', 130000, 2),  --4
+    ('Account Manager', 170000, 3), --5
+    ('Accountant', 145000, 3),  --6
+    ('Legal Team Leader', 200000, 4), --7
+    ('Lawyer', 175000, 4); --8
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('John', 'Mike', 1, 1);
+    ('John', 'Mike', 2, 5),
+    ('Wyatt', 'Johnson', 4, 6),
+    ('Matthew', 'Winters', 6, 7),
+    ('Nancy', 'Smith', 8, 8),
+    ('Madison', 'Lopaz', 1, null),
+    ('Alex', 'Rogers', 3, null),
+    ('Rachel', 'Taylor', 5, null),
+    ('Noah', 'Stevens', 7, null);
