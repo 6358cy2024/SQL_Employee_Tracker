b4b9cad2cfbede6cda22d76e1e9afbd116703db1
Chris Yanez <cy2019@hotmail.com>
2024-07-30 18:54:20 -0400

    most functions working

--- db/seed.sql
+++ db/seed.sql
@@ -16,7 +16,7 @@ INSERT INTO roles (title, salary, department_id) VALUES
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-    ('John', 'Mike', 2, 5),
+    ('John', 'Mike', 2, 5),--This means Madison is John's manager
     ('Wyatt', 'Johnson', 4, 6),
     ('Matthew', 'Winters', 6, 7),
     ('Nancy', 'Smith', 8, 8),
